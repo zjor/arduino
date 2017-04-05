3029e2eaea35dd5a51ff3ce71aad719c5ec89c64
zjor <zjor.se@gmail.com>
2017-04-05 07:41:01 +0200

    meteo-station: dht11 enabled

--- meteo-station/src/main.cpp
+++ meteo-station/src/main.cpp
@@ -11,22 +11,51 @@ A5 (SCL) -> LCD SCL
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
+#include <dht11.h>
+
+#define DHT11_PIN 7
+
 LiquidCrystal_I2C lcd(0x27, 16, 2);
+dht11 dht11;
+
+void printTemperature(int);
+void printHumidity(int);
+void printCO2(int);
 
 void setup() {
   lcd.init();
   lcd.backlight();
   lcd.clear();
+
+  printTemperature(15);
+  printHumidity(34);
+  printCO2(450);
 }
 
 void loop() {
-  lcd.setCursor(pos, 0);
-  lcd.print(line);
-
-  lcd.setCursor(pos2, 1);
-  lcd.print(line2);
+  int chk = dht11.read(DHT11_PIN);
+  printTemperature(dht11.temperature);
+  printHumidity(dht11.humidity);
+  delay(3000);
 }
 
 void printTemperature(int t) {
+  char buffer[16];
+  sprintf(buffer, "T=%d%cC", t, 223);
+  lcd.setCursor(0, 0);
+  lcd.print(buffer);
+}
 
+void printHumidity(int h) {
+  char buffer[16];
+  sprintf(buffer, "H=%d%%", h);
+  lcd.setCursor(8, 0);
+  lcd.print(buffer);
+}
+
+void printCO2(int co2) {
+  char buffer[16];
+  sprintf(buffer, "CO2=%dppm", co2);
+  lcd.setCursor(0, 1);
+  lcd.print(buffer);
 }
