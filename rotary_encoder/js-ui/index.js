02d4bf8b321e7571eca8bff1b59b98d2a34045f6
Sergey Royz <zjor.se@gmail.com>
2019-06-03 18:53:34 +0200

    encoder frontend

--- rotary_encoder/js-ui/index.js
+++ rotary_encoder/js-ui/index.js
@@ -1,3 +1,7 @@
+const app = require('express')()
+const http = require('http').createServer(app)
+const io = require('socket.io')(http)
+
 const SerialPort = require('serialport')
 const Readline = require('@serialport/parser-readline')
 
@@ -7,5 +11,22 @@ const port = new SerialPort(path, { baudRate: 115200 })
 const parser = new Readline()
 port.pipe(parser)
 
-parser.on('data', line => console.log(`> ${line}`))
+parser.on('data', line => {
+    console.log(`> ${line}`)
+    io.emit('data', line)
+})
+
 port.write('ROBOT POWER ON\n')
+
+
+app.get('/', function(req, res){
+ res.sendFile(__dirname + '/index.html')
+})
+
+io.on('connection', function(socket){
+    console.log('Incoming connection')  
+})
+
+http.listen(3000, function(){
+  console.log('listening on *:3000')
+})
