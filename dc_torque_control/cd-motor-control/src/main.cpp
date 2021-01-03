335681514df304b1594939a04adb47cbd64ee042
Sergey Royz <zjor.se@gmail.com>
2021-01-03 13:42:47 +0100

    using INA219

--- dc_torque_control/cd-motor-control/src/main.cpp
+++ dc_torque_control/cd-motor-control/src/main.cpp
@@ -1,22 +1,14 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_INA219.h>
 
 #define OUTPUT_A PD3
 #define OUTPUT_B PD2
 
-#define ADC_PIN 0
-
 #define PWM_PIN 9
 #define DIR_PIN 4
 
-#define BUF_SIZE  3
 #define PPR 5000
-
-volatile int adcReading;
-volatile boolean adcDone;
-boolean adcStarted;
-int value;
-int buf[BUF_SIZE];
-int buf_pos = 0;
 
 int motor_dir = HIGH;
 
@@ -25,32 +17,21 @@ volatile long lastEncoded = 0L;
 
 void encoderHandler();
 
-float avg(int *a, int len) {
-  float sum = 0;
-  for (int i = 0; i < len; i++) {
-    sum += a[i];
-  }
-  return sum / len;
-}
+Adafruit_INA219 ina219;
 
 void log_state() {
-  // current reading
-  Serial.print(avg(buf, BUF_SIZE) - 512);
+  float amps = ina219.getCurrent_mA();
+  Serial.print(amps, 2);
   Serial.print("\t");
-  // Serial.println(encoderValue);
-  Serial.println(0);
+  Serial.println(2 * 3.14 * encoderValue / PPR);
 }
 
-void adc_init() {
+void set_pwm_frequency() {
 //  TCCR1B = TCCR1B & B11111000 | B00000101;    // set timer 1 divisor to  1024 for PWM frequency of    30.64 Hz  
 //  TCCR1B = TCCR1B & B11111000 | B00000100;    // set timer 1 divisor to   256 for PWM frequency of   122.55 Hz  
 //  TCCR1B = TCCR1B & B11111000 | B00000011;    // set timer 1 divisor to    64 for PWM frequency of   490.20 Hz (The DEFAULT)
-//  TCCR1B = TCCR1B & B11111000 | B00000010;    // set timer 1 divisor to     8 for PWM frequency of  3921.16 Hz
-  
+//  TCCR1B = TCCR1B & B11111000 | B00000010;    // set timer 1 divisor to     8 for PWM frequency of  3921.16 Hz  
   TCCR1B = TCCR1B & B11111000 | B00000001;    // set timer 1 divisor to     1 for PWM frequency of 31372.55 Hz
-  ADCSRA =  bit (ADEN);                     
-  ADCSRA |= bit (ADPS2);
-  ADMUX  =  bit (REFS0) | (ADC_PIN & 0x07);
 }
 
 void setup() {
@@ -63,28 +44,16 @@ void setup() {
   pinMode(PWM_PIN, OUTPUT);
   pinMode(DIR_PIN, OUTPUT);
 
-  adc_init();
-
-  Serial.begin(9600);  
+  set_pwm_frequency();
+  ina219.begin();
+  Serial.begin(115200);  
 }
 
 int log_i = 0;
 
 void loop() {
-  if (adcDone) {
-    adcStarted = false;
-    value = adcReading;
-    buf[buf_pos] = value;
-    buf_pos = (buf_pos + 1) % BUF_SIZE;
-    adcDone = false;
-  }
-    
-  if (!adcStarted) {
-    adcStarted = true;
-    ADCSRA |= bit (ADSC) | bit (ADIE);
-  }
 
-  // analogWrite(PWM_PIN, 128);
+  analogWrite(PWM_PIN, 128);
   if (encoderValue >= PPR) {
     motor_dir = LOW;
   }
@@ -95,7 +64,7 @@ void loop() {
   digitalWrite(DIR_PIN, motor_dir);
   
   log_i++;
-  if (log_i > 500) {
+  if (log_i > 5000) {
     log_state();
     log_i = 0;
   }
@@ -117,9 +86,4 @@ void encoderHandler() {
   }
 
   lastEncoded = encoded; //store this value for next time  
-}
-
-ISR (ADC_vect) {
-  adcReading = ADC;
-  adcDone = true;  
 }
