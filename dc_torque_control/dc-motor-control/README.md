4e03e8a1ebbcdb66747b96fcbcd30b6c22b57f8a
Sergey Royz <zjor.se@gmail.com>
2021-01-05 00:09:11 +0100

    position control

--- dc_torque_control/dc-motor-control/README.md
+++ dc_torque_control/dc-motor-control/README.md
@@ -6,9 +6,5 @@ INA219 current reading with Arduino UNO takes ~1008µs or ~1ms
 
 ## Tasks
 
-- develop arduino time-bound routine, executing approx at the same rate, passing dt as an argument
-- control current with PI regulator
-- read absolute angular velocity
-- control velocity with a double feedback loop
-- control motor position with a tripple feedback loop
 - use DC775
+- use potentiometer to set position target
--- dc_torque_control/dc-motor-control/src/main.cpp
+++ dc_torque_control/dc-motor-control/src/main.cpp
@@ -17,6 +17,10 @@
 #define PPR 5000
 
 Encoder encoder(3, 2);
+float last_theta = 0;
+float theta_u = 0;
+float omega = 0;
+float omega_u = 0;
 Adafruit_INA219 ina219;
 
 #define CURRENT_BUF_SIZE  2
@@ -25,7 +29,9 @@ AvgValue current_ma(current_buf, CURRENT_BUF_SIZE);
 float current_avg;
 float u;
 
-PID current_pid(0.15, 0.0, 0.7, 350.0);
+PID current_pid(0.17, 0.0, 0.75, .0);
+PID velocity_pid(40.0, 1.2, 50.0, .0);
+PID position_pid(35.0, 3.0, 20.0, 1.5 * M_PI);
 
 void log_state();
 
@@ -47,19 +53,30 @@ void setup() {
   Serial.begin(115200);
 }
 
-void run(unsigned long now, unsigned long dt) {
+void run(unsigned long now, unsigned long dt_millis) {
+  float dt = 1e-3 * dt_millis;
+  
+  float theta = M_PI * encoder.read() / PPR;
+  omega = (theta - last_theta) / dt;
+  last_theta = theta;
+
+  theta_u = position_pid.getControl(theta, dt);
+  velocity_pid.setTarget(theta_u);
+
+  omega_u = -velocity_pid.getControl(omega, dt);
+  current_pid.setTarget(omega_u);
+
   current_avg = current_ma.get_avg();
 
-  u = current_pid.getControl(current_avg, 1.0 * dt * 1e-3);
+  u = current_pid.getControl(current_avg, dt);
 
   digitalWrite(DIR_PIN, u > 0 ? HIGH : LOW);
-  // digitalWrite(DIR_PIN, HIGH);
   analogWrite(PWM_PIN, (int) min(abs(u), 255));
 
-  log_state();  
+  // log_state();  
 }
 
-TimedTask run_task(&run, 10);
+TimedTask run_task(&run, 1);
 
 void loop() {
   run_task.loop();
@@ -71,6 +88,14 @@ int log_i = 0;
 void log_state() {
   log_i++;
   if (log_i > 10) {
+    Serial.print(last_theta);
+    Serial.print("\t");
+    Serial.print(theta_u);
+    Serial.print("\t");
+    Serial.print(omega);
+    Serial.print("\t");
+    Serial.print(omega_u);
+    Serial.print("\t");
     Serial.print(current_avg);
     Serial.print("\t");
     Serial.println(u);
