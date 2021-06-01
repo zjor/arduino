e2955839fc1c2e69087750a17fe07b6532094d8e
Sergey Royz <zjor.se@gmail.com>
2021-06-01 11:12:25 +0200

    control stepper with timer imterrupts

--- stepper_control_with_interrupts/stepper_control_with_interrupts.ino
+++ stepper_control_with_interrupts/stepper_control_with_interrupts.ino
@@ -1,30 +1,37 @@
 /**
- * Runs stepper interrupt at 20kHz:
- * - compare match register = 16MHz / (prescaler * 20kHz) - 1 => 99, prescaler = 8, 
+ * Runs stepper interrupt at 200kHz:
+ * - compare match register = 16MHz / (prescaler * 200kHz) - 1 => 10, prescaler = 8, 
  * - possible to use 8-bit timers
  * 
- * Runs velocity update at 1kHz:
- * - OCR*A = 16MHz / (prescaler * 1kHz) - 1 = 249, prescaler = 64
+ * Runs velocity update at 5kHz:
+ * - OCR*A = 16MHz / (prescaler * 1kHz) - 1 = 49, prescaler = 64
  * References: 
  * - https://www.instructables.com/Arduino-Timer-Interrupts/
  * - https://sites.google.com/site/qeewiki/books/avr-guide/timers-on-the-atmega328
  */
 
-void setTimer1At20kHz() {
+#define DIR_PIN   5
+#define STEP_PIN  6
+
+#define PPR   1600
+#define TICKS_PER_SECOND  200000 // 200kHz
+#define PULSE_WIDTH 1
+
+void setTimer1(int ocra) {  
   TCCR1A = 0; // set entire TCCR1A register to 0
   TCCR1B = 0; // same for TCCR1B
   TCNT1  = 0; // initialize counter value to 0  
-  OCR1A = 99; // set compare match register for 1hz increments: (16e6) / (20e3 * 8) - 1  
+  OCR1A = ocra;
   TCCR1B |= (1 << WGM12); // turn on CTC mode
   TCCR1B |= (1 << CS11);  // set prescaler to 8  
   TIMSK1 |= (1 << OCIE1A);  // enable timer compare interrupt
 }
 
-void setTimer2At1kHz() {
+void setTimer2(int ocra) {
   TCCR2A = 0; // set entire TCCR1A register to 0
   TCCR2B = 0; // same for TCCR1B
   TCNT2  = 0; // initialize counter value to 0  
-  OCR2A = 249; // set compare match register for 1hz increments: (16e6) / (1e3 * 64) - 1  
+  OCR2A = ocra;
   TCCR2A |= (1 << WGM21); // turn on CTC mode
   TCCR2B |= (1 << CS22);  // set prescaler to 64  
   TIMSK2 |= (1 << OCIE2A);  // enable timer compare interrupt
@@ -33,35 +40,64 @@ void setTimer2At1kHz() {
 
 void setTimers() {
   cli();
-  setTimer1At20kHz();
-  setTimer2At1kHz();
+  setTimer1(9); // 200kHz
+  setTimer2(49); //5kHz
   sei();
 }
 
-volatile uint64_t t1 = 0;
-volatile uint64_t t2 = 0;
+volatile unsigned long currentTick = 0UL;
+volatile unsigned long ticksPerPulse = UINT64_MAX;
+volatile float accel = 0.0;
+volatile float velocity = 0.0;
+
+const float accelStep = 10.0;
+const float maxVelocity = 20.0;
 
 void setup() {
   Serial.begin(115200);
-  setTimers();  
+  setTimers();
+  pinMode(DIR_PIN, OUTPUT);
+  pinMode(STEP_PIN, OUTPUT);
+  digitalWrite(STEP_PIN, LOW);
+  accel = accelStep;
 }
 
 void loop() {
-  if (t1 >= 20e3) {
-    t1 = 0;
-    Serial.println("Timer1 counted to 20k");
+  if (velocity > maxVelocity) {
+    accel = -accelStep;
   }
 
-  if (t2 >= 1000) {
-    t2 = 0;
-    Serial.println("Timer2 couned to 1000");
+  if (velocity < -maxVelocity) {
+    accel = accelStep;
   }
 }
 
+/**
+ * Stepper control interrupt handler
+ */
 ISR(TIMER1_COMPA_vect) {
-  t1++;
+  if (currentTick >= ticksPerPulse) {
+    currentTick = 0;
+  }
+
+  if (currentTick == 0) {
+    digitalWrite(STEP_PIN, HIGH);
+  } else if (currentTick == PULSE_WIDTH) {
+    digitalWrite(STEP_PIN, LOW);
+  }
+  
+  currentTick++;
 }
 
+/**
+ * Velocity update interrupt handler
+ */
 ISR(TIMER2_COMPA_vect) {
-  t2++;
+  velocity += accel * 2e-4;
+  if (abs(velocity) < 1e-3) {
+    ticksPerPulse = UINT64_MAX;
+  } else {
+    ticksPerPulse = (uint64_t)(2.0 * PI * TICKS_PER_SECOND / (abs(velocity) * PPR)) - PULSE_WIDTH;
+  }
+  digitalWrite(DIR_PIN, velocity > 0 ? HIGH : LOW);
 }
