7b414046fe13b9b484e345fbbd3bc2a439f8fe3a
Sergey Royz <zjor.se@gmail.com>
2022-07-09 22:36:09 +0200

    connecting to wifi and send a test message

--- iot-toggle/src/main.cpp
+++ iot-toggle/src/main.cpp
@@ -4,6 +4,23 @@
 
 #include "credentials.h"
 
+const char *url = "https://mqtt2telegram.projects.royz.cc/api/v1.0/send";
+const char *JSON_TEMPLATE = "{\"topic\":\"%s\",\"payload\":\"%s\"}";
+const char *topic = "toggle/101";
+
+// TODO: move to bot_client library
+void send_message(const char *topic, const char *payload) {
+  HTTPClient http;
+  http.begin(url);
+  http.addHeader("Content-Type", "application/json");
+  http.setAuthorization(BOT_LOGIN, BOT_PASSWORD);
+
+  char requestBody[256];
+    
+  snprintf_P(requestBody, sizeof(requestBody), PSTR(JSON_TEMPLATE), topic, payload);
+  int responseCode = http.POST(requestBody);
+  Serial.println(responseCode);
+}
 
 void setup() {
 
@@ -16,18 +33,14 @@ void setup() {
   while (wifi_status != WL_CONNECTED) {
     wifi_status = WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
     Serial.print(".");    
-    delay(1500);
+    delay(5000);
   }
 
   Serial.println("\nConnected!");
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
 
-  HTTPClient http_client;
-  // TODO: https://github.com/espressif/arduino-esp32/blob/master/libraries/HTTPClient/examples/BasicHttpClient/BasicHttpClient.ino
-  // http_client.begin("");
-  // http_client.addHeader()
-  // http_client.POST()
+  send_message(topic, "Hello world");
 
 }
 
