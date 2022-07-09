c13cb58f1e71a663a2ef54b13261f108f8d0ea26
Sergey Royz <zjor.se@gmail.com>
2022-07-09 23:02:10 +0200

    sending toggle state

--- iot-toggle/src/main.cpp
+++ iot-toggle/src/main.cpp
@@ -4,9 +4,18 @@
 
 #include "credentials.h"
 
+#define R_PIN 14
+#define G_PIN 15
+#define B_PIN 27
+
+#define SWITCH_A_PIN  12
+#define SWITCH_B_PIN  13
+
 const char *url = "https://mqtt2telegram.projects.royz.cc/api/v1.0/send";
 const char *JSON_TEMPLATE = "{\"topic\":\"%s\",\"payload\":\"%s\"}";
 const char *topic = "toggle/101";
+
+int toggle_a_state, toggle_b_state;
 
 // TODO: move to bot_client library
 void send_message(const char *topic, const char *payload) {
@@ -22,7 +31,31 @@ void send_message(const char *topic, const char *payload) {
   Serial.println(responseCode);
 }
 
+void update_led() {
+  if (toggle_a_state == LOW) {
+    digitalWrite(R_PIN, HIGH);
+    digitalWrite(G_PIN, LOW);
+  } else {
+    digitalWrite(R_PIN, LOW);
+    digitalWrite(G_PIN, HIGH);
+  }
+}
+
 void setup() {
+
+  pinMode(R_PIN, OUTPUT);
+  pinMode(G_PIN, OUTPUT);
+  pinMode(B_PIN, OUTPUT);
+  
+  digitalWrite(R_PIN, HIGH);
+  digitalWrite(G_PIN, HIGH);
+  digitalWrite(B_PIN, HIGH);
+
+  pinMode(SWITCH_A_PIN, INPUT_PULLUP);
+  pinMode(SWITCH_B_PIN, INPUT_PULLUP);
+  
+  toggle_a_state = digitalRead(SWITCH_A_PIN);
+  toggle_b_state = digitalRead(SWITCH_B_PIN);  
 
   Serial.begin(115200);
 
@@ -30,20 +63,36 @@ void setup() {
 
   Serial.println("Connecting to WiFi...");
 
+  static int b_state = LOW;
   while (wifi_status != WL_CONNECTED) {
     wifi_status = WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-    Serial.print(".");    
+    Serial.print(".");
+    
+    digitalWrite(B_PIN, b_state);
+    b_state = !b_state;
+
     delay(5000);
   }
+
+  digitalWrite(B_PIN, HIGH);
 
   Serial.println("\nConnected!");
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
 
-  send_message(topic, "Hello world");
-
+  send_message(topic, "online");
+  update_led();
 }
 
 void loop() {
+  int new_a_state = digitalRead(SWITCH_A_PIN);
+  if (new_a_state != toggle_a_state) {
+    toggle_a_state = new_a_state;
+    update_led();
+
+    send_message(topic, toggle_a_state == LOW ? "idle" : "busy");
+  }
+
+  delay(50);
   
 }
