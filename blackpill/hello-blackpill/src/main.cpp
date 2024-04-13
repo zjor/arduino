0756a8e014860a7f96c6dda4a3dc9740d463ba82
Sergey Royz <zjor.se@gmail.com>
2024-04-13 14:44:11 +0200

    simple blinker

--- blackpill/hello-blackpill/src/main.cpp
+++ blackpill/hello-blackpill/src/main.cpp
@@ -1,53 +1,24 @@
+#include <math.h>
 #include <Arduino.h>
-#include <Wire.h>
- 
-// TwoWire Wire2;
-// #define Wire Wire2
+
+#define N 64
+
+uint16_t state = HIGH;
+uint32_t delays[N] = {};
 
 void setup() {
-  Wire.begin(PB7, PB6);  
-  Serial.begin(115200);
-}
- 
- 
-void loop()
-{
-  byte error, address;
-  int nDevices;
- 
-  Serial.println("Scanning...");
- 
-  nDevices = 0;
-  for(address = 1; address < 127; address++ )
-  {
-    // The i2c_scanner uses the return value of
-    // the Write.endTransmisstion to see if
-    // a device did acknowledge to the address.
-    Wire.beginTransmission(address);
-    error = Wire.endTransmission();
- 
-    if (error == 0)
-    {
-      Serial.print("I2C device found at address 0x");
-      if (address<16)
-        Serial.print("0");
-      Serial.print(address,HEX);
-      Serial.println("  !");
- 
-      nDevices++;
-    }
-    else if (error==4)
-    {
-      Serial.print("Unknown error at address 0x");
-      if (address<16)
-        Serial.print("0");
-      Serial.println(address,HEX);
-    }    
+  pinMode(PC13, OUTPUT);
+  for (int i = 0; i < N; i++) {
+    delays[i] = (int)(200.0 + 150.0 * sin(2.0 * i * PI / N));
   }
-  if (nDevices == 0)
-    Serial.println("No I2C devices found\n");
-  else
-    Serial.println("done\n");
- 
-  delay(5000);           // wait 5 seconds for next scan
+}
+
+uint16_t i = 0;
+
+void loop() {
+  digitalWrite(PC13, state);
+  state = !state;
+  
+  delay(delays[i]);
+  i = (++i) % N;
 }
